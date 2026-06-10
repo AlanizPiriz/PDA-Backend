2b90315c389927b54dbaeb0ce7a6692866038f39
Alan <alanizpiriz@gmail.com>
2026-06-10 13:34:39 -0300

    Fix cors admin

--- server.js
+++ server.js
@@ -37,12 +37,16 @@ function saveStores(data) {
 const app = express();
 app.use(cors({
   origin: [
-    "https://alanizpiriz.github.io",
+    "http://127.0.0.1:3001",
+    "http://localhost:3001",
+    "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "http://127.0.0.1:5500"
+    "https://alanizpiriz.github.io"
   ],
-  credentials: true
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "x-admin-key"]
 }));
+
 app.use(express.json());
 
 
